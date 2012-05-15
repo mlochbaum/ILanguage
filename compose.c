bd12d83651138d5521aa4f0c564353962f1ccba4
Marshall Lochbaum <mwlochbaum@gmail.com>
2012-05-14 23:26:30 -0400

    More compositions

--- compose.c
+++ compose.c
@@ -1,5 +1,9 @@
+D_F1(left)  { return l; }
+D_F2(left)  { return l; }
+D_F2(right) { return r; }
+
 D_F2(apply) { return apply1(r,l); }
-D_F2(flip) { return apply1(l,r); }
+D_F2(flip)  { return apply1(l,r); }
 D_F2(compose) { DECL_ARR(V,v,1); v[0]=l; return makeO(r,1,v); }
 
 D_F11(flip)  { return apply2(l,ll,ll); }
@@ -8,10 +12,15 @@ D_F12(flip)  { return apply2(l,rr,ll); }
 D_F21(bind)  { return apply2(l,ll,r); }
 D_F21(rbind) { return apply2(r,l,ll); }
 
+// TODO better name
 D_F21(compose) { return apply1(r, apply1(l,ll)); }
 D_F22(compose) { return apply2(r, apply1(l,ll), apply1(cpy(l),rr)); }
 
 void compose_init() {
+  B_f1['['] = B_f1[']'] = &left_f1;
+  B_f2['['] = &left_f2;
+  B_f2[']'] = &right_f2;
+
   B_f2['.'] = &apply_f2;
   B_f2['~'] = &flip_f2;
   B_f2['o'] = &compose_f2;
