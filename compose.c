b6f710c5f8fa0fc7501bd5e47ba777577580fad7
Marshall Lochbaum <mwlochbaum@gmail.com>
2012-05-16 16:20:25 -0400

    Power, while

--- compose.c
+++ compose.c
@@ -18,6 +18,27 @@ D_F21(rbind) { return apply2(r,l,ll); }
 D_F21(compose) { return apply1(r, apply1(l,ll)); }
 D_F22(compose) { return apply2(r, apply1(l,ll), apply1(cpy(l),rr)); }
 
+// TODO domain
+D_F21(power) {
+  I n=getFloorZ(r); del(r); DDO(i,n) ll=apply1(cpy(l),ll);
+  del(l); return ll;
+}
+D_F22(power) {
+  I n=getFloorZ(r); del(r); DDO(i,n) ll=apply2(cpy(l),ll,cpy(rr));
+  del(l); del(rr); return ll;
+}
+
+I toBool(V v) { I r = (v->t!=Z_t || *(Z)v->v); del(v); return r; }
+D_F21(while) {
+  while(toBool(apply1(cpy(r), cpy(ll)))) ll=apply1(cpy(l),ll);
+  del(l); del(r); return ll;
+}
+D_F22(while) {
+  while(toBool(apply2(cpy(r), cpy(ll), cpy(rr))))
+    ll=apply2(cpy(l),ll,cpy(rr));
+  del(l); del(r); del(rr); return ll;
+}
+
 EXTERN_BUILTINS;
 void compose_init() {
   B_f1['['] = B_f1[']'] = &left_f1;
@@ -37,4 +58,10 @@ void compose_init() {
 
   B_f21['b'] = &bind_f21;
   B_f21['B'] = &rbind_f21;
+
+  B_f21['p'] = &power_f21;
+  B_f22['p'] = &power_f22;
+
+  B_f21['w'] = &while_f21;
+  B_f22['w'] = &while_f22;
 }
