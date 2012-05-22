83fe4de69ad1d00ff724dadd8112256260c3b1b5
Marshall Lochbaum <mwlochbaum@gmail.com>
2012-05-21 23:21:47 -0400

    Add eval

--- string.c
+++ string.c
@@ -1,7 +1,18 @@
 Str toString(V);
+V scan(Str);
 D_F1(print) { V v=makeStr(toString(l)); del(l); return v; }
+
+D_D1(string) { return !!((l->t&A_t) && (LIST_T(l)==S_t)); }
+Str arrToString(I l, I c, I o, Ptr v) {
+  DECL_NSTR(s, l); memcpy(s,v+o,min(c-o,l)); memcpy(s,v,max(0,l+o-c));
+}
+D_F1(eval) { A a=l->v; Str s=arrToString(a->l, a->c, a->o, a->v);
+  del(l); return scan(s);
+}
 
 EXTERN_BUILTINS;
 void string_init() {
   B_f1['s'] = &print_f1;
+  B_d1['S'] = &string_d1;
+  B_f1['S'] = &eval_f1;
 }
