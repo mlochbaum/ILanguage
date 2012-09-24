1d053b17c10f8282a8c34cd4a7d2b8900a57ed24
Marshall Lochbaum <mwlochbaum@gmail.com>
2012-09-23 23:23:46 -0400

    Soft typing in name.c

--- name.c
+++ name.c
@@ -5,21 +5,28 @@ typedef StrV_HashMap Name;
 
 Name names;
 
+D_S2(set) { return 2 + !!(l&N_t); }
 D_D2(set) { return 2 + !!(T(l)&N_t); }
-D_F2(set) { StrVset(names, strdup(N(l)), r); return l; }
+D_T2(set) { return N_t; }
+D_P2(set) { StrVset(names, strdup(N(l)), r); *(N*)p=strdup(N(l)); }
 
+D_S1(name) { return !!(l&N_t); }
 D_D1(name) { return !!(T(l)&N_t); }
-D_F1(get) { V v=StrVget(names, N(l)); del(l); return cpy(v); }
-D_F1(del) { StrVdel(names, N(l)); return l; }
+D_P1(get) { V v=StrVget(names, N(l)); del(l); *(V*)p=cpy(v); }
+D_T1(del) { return N_t; }
+D_P1(del) { StrVdel(names, N(l)); *(N*)p=N(l); }
 
 EXTERN_BUILTINS;
 void name_init() {
   names = StrVnew(256);
 
+  B_s2[':'] = &set_s2;
+  B_t2[':'] = &set_t2;
   B_d2[':'] = &set_d2;
-  B_f2[':'] = &set_f2;
+  B_p2[':'] = &set_p2;
 
+  B_s1['.'] = B_s1['d'] = &name_s1;
   B_d1['.'] = B_d1['d'] = &name_d1;
-  B_f1['.'] = &get_f1;
-  B_f1['d'] = &del_f1;
+  B_t1['.'] = &v_t1;   B_p1['.'] = &get_p1;
+  B_t1['d'] = &del_t1; B_p1['d'] = &del_p1;
 }
