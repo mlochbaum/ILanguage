90fedc2d6ab365f034b816802b8ee40e7197783f
Marshall Lochbaum <mwlochbaum@gmail.com>
2012-10-16 15:13:15 -0400

    Correct compose.c for new apply

--- compose.c
+++ compose.c
@@ -9,33 +9,33 @@ D_P12(constant) { del(ll); del(rr); RE(l); }
 
 D_P2(apply) { V(p) = apply1d(r,l); }
 D_P2(flip)  { V(p) = apply1d(l,r); }
-D_P2(compose) { DECL_ARR(V,v,1); v[0]=l; setO(p, wrapO(r,1,v)); }
-D_P11(compose) { DECL_ARR(V,v,1); v[0]=ll; setO(p, wrapO(cpy(l),1,v)); }
-D_P12(compose) { DECL_ARR(V,v,2); v[0]=ll; v[1]=rr; setO(p, wrapO(cpy(l),2,v)); }
+D_P2(compose) { DECL_ARR(V,v,1); v[0]=cpy1(l); setO(p, wrapO(cpy1(r),1,v)); }
+D_P11(compose) { DECL_ARR(V,v,1); v[0]=cpy1(ll); setO(p, wrapO(cpy(l),1,v)); }
+D_P12(compose) { DECL_ARR(V,v,2); v[0]=cpy1(ll); v[1]=cpy1(rr); setO(p, wrapO(cpy(l),2,v)); }
 
 D_T11(flip)  { return apply2_T(l,ll,ll); }
-D_P11(flip)  { apply2_P(p,l,cpy(ll),ll); }
+D_P11(flip)  { V l1=cpy(ll); apply2_P(p,l,l1,ll); FREE(P(l1)); }
 D_T12(flip)  { return apply2_T(l,rr,ll); }
 D_P12(flip)  { apply2_P(p,l,rr,ll); }
 
 D_T21(bind)     { return apply2_T(l,ll,T(r)); }
-D_P21(bind)     { apply2_P(p,l,ll,cpy(r)); }
+D_P21(bind)     { V r_=cpy(r); apply2_P(p,l,ll,r_); FREE(P(r_)); }
 D_T21(backbind) { return apply2_T(r,T(l),ll); }
-D_P21(backbind) { apply2_P(p,r,cpy(l),ll); }
+D_P21(backbind) { V l_=cpy(l); apply2_P(p,r,l_,ll); FREE(P(l_)); }
 
 D_T21(hook)     { return apply2_T(r,apply1_T(l,ll),ll); }
-D_P21(hook)     { apply2_P(p,r,apply1(l,cpy(ll)),ll); }
+D_P21(hook)     { V l1,a; apply2_P(p,r,a=apply1(l,l1=cpy(ll)),ll); FREE(P(l1)); FREE(P(a)); }
 D_T22(hook)     { return apply2_T(r,apply1_T(l,ll),rr); }
 D_P22(hook)     { apply2_P(p,r,apply1(l,ll),rr); }
 D_T21(backhook) { return apply2_T(l,ll,apply1_T(r,ll)); }
-D_P21(backhook) { apply2_P(p,l,ll,apply1(r,cpy(ll))); }
+D_P21(backhook) { V l1,a; apply2_P(p,l,ll,a=apply1(r,l1=cpy(ll))); FREE(P(l1)); FREE(P(a)); }
 D_T22(backhook) { return apply2_T(l,ll,apply1_T(r,rr)); }
 D_P22(backhook) { apply2_P(p,l,ll,apply1(r,rr)); }
 
 D_T21(compose) { return apply1_T(r, apply1_T(l,ll)); }
-D_P21(compose) { apply1_P(p, r, apply1(l,ll)); }
+D_P21(compose) { V a; apply1_P(p, r, a=apply1(l,ll)); FREE(P(a)); }
 D_T22(compose) { return apply2_T(r, apply1_T(l,ll), apply1_T(l,rr)); }
-D_P22(compose) { apply2_P(p, r, apply1(l,ll), apply1(l,rr)); }
+D_P22(compose) { V al,ar; apply2_P(p, r, al=apply1(l,ll), ar=apply1(l,rr)); FREE(P(al)); FREE(P(ar)); }
 
 I toBool(V v) { I r = (T(v)!=Z_t || Z(v)); del(v); return r; }
 /* TODO
@@ -82,7 +82,7 @@ void compose_init() {
   DB(t2,'o',O);  DB(p2,'o',compose);
   DB(t11,'o',O); DB(p11,'o',compose);
   DB(t12,'o',O); DB(p12,'o',compose);
-  D(22,'O',compose);
+  D(21,'O',compose);  D(22,'O',compose);
   D(21,'h',hook);     D(22,'h',hook);
   D(21,'H',backhook); D(22,'H',backhook);
 
