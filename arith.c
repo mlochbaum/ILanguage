502f8929cb3c53a3f26665f16cf27d85577a1621
Marshall Lochbaum <mwlochbaum@gmail.com>
2012-10-30 22:03:17 -0400

    Implement S for arithmetic dyads

--- arith.c
+++ arith.c
@@ -21,23 +21,43 @@ OP(ceiling, ceiling);
 #undef OP
 
 //Dyads
-#define OP(op) { switch (max(T(l),T(r))) { DL(Z,op); DL(R,op); } del(l);del(r); }
-#define DL(T, op) case T##_t: set##T(p, get##T(l)op get##T(r)); break
-D_P2(plus) OP(+);
-D_P2(minus) OP(-);
-D_P2(times) OP(*);
-D_P2(divide) OP(/);
-#undef DL
-#define DL(T, op) case T##_t: set##T(p, op(get##T(l), get##T(r))); break
-#define MOD(l,r) (l) - (r)*floor((l)/(r))
-#define DL1(T, op) case T##_t: set##T(p, get##T(l)op get##T(r)); break
-// D_P2(mod) OP(MOD);
-D_P2(mod) { switch (max(T(l),T(r))) { DL1(Z,%); DL(R,MOD); } del(l);del(r); }
-#undef DL1
-#undef MOD
-D_P2(min) OP(min);
-D_P2(max) OP(max);
-#undef DL
+#define ON(op,l,r) (l) op (r)
+#define DFZZ(n, op) D_S(n##ZZ) { return setZ(v,ON(op,Z(vs[0]),Z(vs[1]))); }
+#define DFZR(n, op) D_S(n##ZR) { return setR(v,ON(op,(R)Z(vs[0]),R(vs[1]))); }
+#define DFRZ(n, op) D_S(n##RZ) { return setR(v,ON(op,Z(vs[0]),(R)Z(vs[1]))); }
+#define DFRR(n, op) D_S(n##RR) { return setR(v,ON(op,R(vs[0]),R(vs[1]))); }
+
+#define LINE(n, T1,T2) case T2##_t: s.f=&n##T1##T2##_s; break;
+#define DECL_S2(n)  D_S2(n) { S s; switch(l){ \
+    case Z_t: switch(r){ LINE(n,Z,Z); LINE(n,Z,R); } \
+    case R_t: switch(r){ LINE(n,R,Z); LINE(n,R,R); } } return s; }
+#define OP(n,op)  DFZZ(n,op) DFZR(n,op) DFRZ(n,op) DFRR(n,op) DECL_S2(n)
+OP(plus, +);
+OP(minus, -);
+OP(times, *);
+OP(divide, /);
+#undef ON
+#define ON(op,l,r) op(l,r)
+OP(min, min);
+OP(max, max);
+#undef ON
+
+Z modZZ(Z a, Z b) { Z m=a%b; return m+b*(m*b<0); }
+R modRZ(R a, Z b) { return a-b*floor(a/b); }
+D_S(modZZ) { return setZ(v,modZZ(Z(vs[0]),Z(vs[1]))); }
+D_S(modRZ) { return setR(v,modRZ(R(vs[0]),Z(vs[1]))); }
+D_S2(mod) { S s; switch(r) { // Using r, not l
+  case Z_t: { switch(l){ case Z_t: s.f=&modZZ_s; break;
+                         case R_t: s.f=&modRZ_s; break; }
+              return s; }
+  case R_t: s.f=NULL; return s;
+} }
+
+#undef DFZZ
+#undef DFZR
+#undef DFRZ
+#undef DFRR
+#undef LINE
 #undef OP
 
 // EXPORT DEFINITIONS
@@ -52,7 +72,7 @@ void arith_init() {
 #undef SET
 
 #define SET(c, f) B_l2[c] = B_u2[c] = &arith_l2; B_t2[c] = &arith##_t2; \
-                  B_d2[c] = &arith_d2; B_p2[c] = &f##_p2
+                  B_d2[c] = &arith_d2; B_s2[c] = &f##_s2
   SET('+', plus);
   SET('-', minus);
   SET('*', times);
