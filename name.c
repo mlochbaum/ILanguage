mlochbaum/ILanguage

commit fddadc6af788f78cbe8a0889af9d61d409e5307f

Make a failed name lookup return null, not an error (fixes memory issues)

name.c
@@ -3,7 +3,7 @@
 #include "name.h"
 
 I equalsStr(Str l, Str r) { return 0==strcmp(l,r); }
-SET_HASH_TABLE(Str, V, hash_string, equalsStr, FREE, ddel, Err(""));
+SET_HASH_TABLE(Str, V, hash_string, equalsStr, FREE, ddel, ((V){0,0}));
 
 D_L2(set) { return 2 + !!(l&N_t); }
 D_D2(set) { return 2 + !!(T(l)&N_t); }
@@ -12,7 +12,10 @@ D_P2(set) { StrVset(names, strdup(N(l)), cpy1(r)); setN(p, N(l)); }
 
 D_L1(name) { return !!(l&N_t); }
 D_D1(name) { return !!(T(l)&N_t); }
-D_P1(get) { V v=StrVget(names, N(l)); del(l); mv_P(p,cpy(v)); }
+D_P1(get) {
+  V v=StrVget(names, N(l)); del(l);
+  if (P(v)) mv_P(p, cpy(v)); else setE(p, strdup("Value error"));
+}
 D_T1(del) { return N_t; }
 D_P1(del) { StrVdel(names, N(l)); setN(p,N(l)); }
 

apply.c
@@ -91,9 +91,14 @@ void apply_P_F(V v, F f, I n, V* x) {
   }
 }
 
-T apply_T_N(N m, I n, T* x) { return apply_T(StrVget(names, m), n, x); }
+T apply_T_N(N m, I n, T* x) {
+  V mv=StrVget(names, m);
+  return P(mv) ? apply_T(mv, n, x) : E_t;
+}
 void apply_P_N(V v, N m, I n, V* x) {
-  return apply_P(v, StrVget(names, m), n, x);
+  V mv=StrVget(names, m);
+  if (!P(mv)) { DDO(i,n) del(x[i]); E(v)=strdup("Value error"); return; }
+  return apply_P(v, mv, n, x);
 }
 
 T apply_T_Q(Q q, I n, T* x) { return E_t; }

spec.c
@@ -14,7 +14,9 @@ V apply_S_L(L f, I n, T* x) {
 }
 
 V apply_S_N(N f, I n, T* x) {
-  return apply_S(StrVget(names, f), n, x);
+  V fv=StrVget(names, f);
+  if (!P(fv)) { return Err("Value error"); }
+  return apply_S(fv, n, x);
 }
 
 V apply_S_B(B,I,T*);

commit b89abaf4b01def7cc727858b7d394e1bc3d64110

Fix memory bug in get_p1 (introduced by fddadc6a)

name.c
@@ -14,7 +14,7 @@ D_L1(name) { return !!(l&N_t); }
 D_D1(name) { return !!(T(l)&N_t); }
 D_P1(get) {
   V v=StrVget(names, N(l)); del(l);
-  if (P(v)) mv_P(p, cpy(v)); else setE(p, strdup("Value error"));
+  if (P(v)) mv_Pd(p, cpy(v)); else setE(p, strdup("Value error"));
 }
 D_T1(del) { return N_t; }
 D_P1(del) { StrVdel(names, N(l)); setN(p,N(l)); }