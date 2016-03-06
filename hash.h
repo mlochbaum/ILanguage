88acf0b1a20cd75bd5ffcd018b8a48117d337e35
Marshall Lochbaum <mwlochbaum@gmail.com>
2016-03-05 20:53:32 -0500

    Style changes to hash.h

--- hash.h
+++ hash.h
@@ -23,15 +23,15 @@ UI hash(V);
     DDO(i,l) m->v[i]=NULL; return m; }                                   \
   void name##freeL(L l) { freeK(l->k); freeV(l->v); FREE(l); }           \
   M name##resize(M m, I l) { M mm=name##new(l);                          \
-    DDO(i,l) { L n=m->v[i],nn; while(n){                                 \
-      name##set(mm, n->k, n->v); nn=n; n=n->n; name##freeL(nn);          \
+    DDO(i,l) { L n=m->v[i]; while(n){                                    \
+      name##set(mm, n->k, n->v); L nn=n; n=n->n; name##freeL(nn);        \
     } } mm->l=l; mm->n=m->n; FREE(m->v); FREE(m); return mm; }           \
   void name##set(M m, K k, V v) {                                        \
     if (m->n==m->l) m=name##resize(m, m->l*2);                           \
     I i=hash(k)%m->l; L n=m->v[i];                                       \
     DECL(L, l); l->k=k; l->v=v; l->n=n; m->v[i]=l; }                     \
-  void name##free(M m) { DDO(i,m->l){ L n=m->v[i],nn;                    \
-    while(n){ nn=n; n=n->n; name##freeL(nn); } } FREE(m->v); FREE(m); }  \
+  void name##free(M m) { DDO(i,m->l){ L n=m->v[i];                       \
+    while(n){ L nn=n; n=n->n; name##freeL(nn); } } FREE(m->v); FREE(m); }\
   V name##get(M m, K k) { L n=m->v[hash(k)%m->l];                        \
     while(n){if(equals(k,n->k))return n->v; else n=n->n;} return (N); }  \
   void name##del(M m, K k) {                                             \
