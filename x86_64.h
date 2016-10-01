f1a7cd5ce2604020a965e0ed296d661d263db3aa
Marshall Lochbaum <mwlochbaum@gmail.com>
2016-10-01 00:15:12 -0400

    Add REX handling for 64-bit operations

--- x86_64.h
+++ x86_64.h
@@ -28,33 +28,34 @@ typedef unsigned short RegM;
 #define REG_SAVE 4087 // Registers which function calls may modify
 
 // Instructions
-#define A_0REG(O,I) (((I)<<3) + (O))
-#define A_REG(O,I) (0xC0 + A_0REG(O,I)) // TODO REX
+#define A_0REG(O,I) ((((I)&7)<<3) + ((O)&7))
+#define A_REG(O,I) (0xC0 + A_0REG(O,I))
+#define REX8(O,I) (0x48 + ((O)>7)+(((I)>7)<<2))
 
-#define MOV(O,I) {0x48,0x89,A_REG(O,I)}
-#define ADD(O,I) {0x48,0x01,A_REG(O,I)}
-#define SUB(O,I) {0x48,0x29,A_REG(O,I)}
-#define IMUL(O,I) {0x48,0x0F,0xAF,A_REG(I,O)}
-#define CMP(O,I) {0x48,0x39,A_REG(O,I)}
-#define CMOVLE(O,I) {0x48,0x0F,0x4E,A_REG(I,O)}
-#define NEG(O,I) {0x48,0xF7,A_REG(I,3)}
+#define MOV(O,I)  {REX8(O,I),0x89,A_REG(O,I)}
+#define ADD(O,I)  {REX8(O,I),0x01,A_REG(O,I)}
+#define SUB(O,I)  {REX8(O,I),0x29,A_REG(O,I)}
+#define IMUL(I,O) {REX8(O,I),0x0F,0xAF,A_REG(O,I)}
+#define CMP(O,I)  {REX8(O,I),0x39,A_REG(O,I)}
+#define CMOVLE(I,O) {REX8(O,I),0x0F,0x4E,A_REG(O,I)}
+#define NEG(I,O)  {REX8(O,0),0xF7,A_REG(O,3)}
 
-#define ADDI(O,I) {0x48,0x83,A_REG(O,0),I}
+#define ADDI(O,I) {REX8(O,0),0x83,A_REG(O,0),I}
 
 #define PUSH(O,I) {0x50+O}
 #define POP(O,I)  {0x58+O}
 
 #define BYTES4(I) ((UC)(I)),((UC)((I)>>8)),((UC)((I)>>16)),((UC)((I)>>24))
 
-#define MOV_MR(O,I,OFF) {0x48,0x89,0x40+A_0REG(O,I),OFF}
+#define MOV_MR(O,I,OFF) {REX8(O,I),0x89,0x40+A_0REG(O,I),OFF}
 #define MOV_MI(O,I,OFF) {0xC7,0x40+A_0REG(O,0),OFF,BYTES4(I)}
-#define MOV_RI(O,I) {0xB8+(O) , BYTES4(I)}
-#define MOV_RM(O,I,OFF) {0x48,0x8B,0x40+A_0REG(I,O),OFF}
-#define MOV_RM0(O,I) {0x48,0x8B,A_0REG(I,O)}
+#define MOV_RI(O,I)     {0xB8+(O) , BYTES4(I)}
+#define MOV_RM(I,O,OFF) {REX8(O,I),0x8B,0x40+A_0REG(O,I),OFF}
+#define MOV_RM0(I,O)    {REX8(O,I),0x8B,A_0REG(O,I)}
 
 #define BYTES8(I) ((UC)(I)),((UC)((I)>>8)),((UC)((I)>>16)),((UC)((I)>>24)) \
           ,((UC)((I)>>32)),((UC)((I)>>40)),((UC)((I)>>48)),((UC)((I)>>56))
-#define MOV_RI8(O,I) {0x48,0xB8+(O) , BYTES8(I)}
+#define MOV_RI8(O,I) {REX8(O,0),0xB8+(O) , BYTES8(I)}
 
 #define CALL(O,I) {0xFF,A_REG(O,2)}
 
@@ -75,7 +76,7 @@ typedef unsigned short RegM;
 #define JLE(O,I) {0x7E,((UC)(O)-2)}
 #define JG(O,I)  {0x7F,((UC)(O)-2)}
 
-#define SUBI4(O,I) {0x48,0x81,A_REG(O,5),BYTES4(I)}
+#define SUBI4(O,I) {REX8(O,0),0x81,A_REG(O,5),BYTES4(I)}
 
 #define RET {0xC3}
 
