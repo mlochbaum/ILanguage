4b468a7c7285150737cfd7d0365098041928c294
Marshall Lochbaum <mwlochbaum@gmail.com>
2016-10-06 23:34:09 -0400

    Remove arith_s2 functions

--- arith.c
+++ arith.c
@@ -8,7 +8,7 @@ D_D2(arith) { return arith_l2(T(l),T(r)); }
 
 D_T2(arith) { return max(l,r); }
 
-//Monads
+// Monads
 D_A1(negate) {
   switch (l) {
     case Z_t: if (choose_reg(a)) ASM(a, MOV,a->o,a->i[0]);
@@ -64,47 +64,8 @@ D_A1(square) {
   }
 }
 
-#define D_S(name) void name##_s(P p, V v, I n, V* vs)
-//Dyads
-#define ON(op,l,r) (l) op (r)
-#define DFZZ(n, op) D_S(n##ZZ) { return setZ(v,ON(op,Z(vs[0]),Z(vs[1]))); }
-#define DFZR(n, op) D_S(n##ZR) { return setR(v,ON(op,(R)Z(vs[0]),R(vs[1]))); }
-#define DFRZ(n, op) D_S(n##RZ) { return setR(v,ON(op,R(vs[0]),(R)Z(vs[1]))); }
-#define DFRR(n, op) D_S(n##RR) { return setR(v,ON(op,R(vs[0]),R(vs[1]))); }
 
-#define LINE(n, T1,T2) case T2##_t: s.t=max(T1##_t,T2##_t); s.f=&n##T1##T2##_s; return s
-#define DECL_S2(n)  D_S2(n) { S s; switch(l){ \
-    case Z_t: switch(r){ LINE(n,Z,Z); LINE(n,Z,R); } \
-    case R_t: switch(r){ LINE(n,R,Z); LINE(n,R,R); } \
-    default: s.f=NULL; return s; } }
-#define OP(n,op)  DFZZ(n,op) DFZR(n,op) DFRZ(n,op) DFRR(n,op) DECL_S2(n)
-OP(plus, +);
-OP(minus, -);
-OP(times, *);
-OP(divide, /);
-#undef ON
-#define ON(op,l,r) op(l,r)
-OP(min, min);
-OP(max, max);
-#undef ON
-
-Z modZZ(Z a, Z b) { Z m=a%b; return m+b*(m*b<0); }
-R modRZ(R a, Z b) { return a-b*floor(a/b); }
-D_S(modZZ) { return setZ(v,modZZ(Z(vs[0]),Z(vs[1]))); }
-D_S(modRZ) { return setR(v,modRZ(R(vs[0]),Z(vs[1]))); }
-D_S2(mod) {
-  S s; s.f=NULL; if (r==Z_t) {
-    switch(l){ case Z_t: s.t=Z_t; s.f=&modZZ_s; break;
-               case R_t: s.t=R_t; s.f=&modRZ_s; break; }
-  } return s;
-}
-
-#undef DFZZ
-#undef DFZR
-#undef DFRZ
-#undef DFRR
-#undef LINE
-#undef OP
+// Dyads
 
 // Make sure both arguments are in xmm registers. Cast if not.
 // Move one argument to the result, and return its index in a->i.
@@ -248,8 +209,8 @@ void arith_init() {
   SET('Q', R, sqroot);
 #undef SET
 
-#define SET(c, f) B_l2[c] = B_u2[c] = &arith_l2; B_t2[c] = &arith##_t2; \
-                  B_d2[c] = &arith_d2; B_s2[c] = &f##_s2; B_a2[c] = &f##_a2
+#define SET(c, f) B_l2[c] = B_u2[c] = &arith_l2; B_t2[c] = &arith_t2; \
+                  B_d2[c] = &arith_d2; B_a2[c] = &f##_a2
   SET('+', plus);
   SET('-', minus);
   SET('*', times);
