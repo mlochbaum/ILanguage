f8cb069040e2073dc9e6475d1ae076021b5d9fa5
Marshall Lochbaum <mwlochbaum@gmail.com>
2018-06-08 18:50:58 +0100

    Update compose_templ.c somewhat

--- ic/compose_templ.c
+++ ic/compose_templ.c
@@ -16,19 +16,21 @@ D_P12(split_compose) {
   setO(p, wrapO(cpy(l),2,v));
 }
 
-I toBoold(V v) { I r = (T(v)!=Z_t || Z(v)); ddel(v); return r; }
-I toBool(V v) { I r = (T(v)!=Z_t || Z(v)); del(v); return r; }
+B toBoold(V v) { B r = (T(v)!=Z_t || Z(v)); ddel(v); return r; }
+B toBool(V v) { B r = (T(v)!=Z_t || Z(v)); del(v); return r; }
 
 D_L2(power) { return 2*(!!(r&Z_t)) + 1; }
 D_D2(power) { return power_l2(T(l), T(r)); }
 D_T21(power) {
-  I n=getFloorZ(r);
-  DO(i,n) { T llt=apply1_T(l,ll); if(llt==ll)break; ll=llt; }
+  Z n=getFloorZ(r); if(n<0) return E_t;
+  DO(i,(U)n) { T llt=apply1_T(l,ll); if(llt==ll)break; ll=llt; }
   return ll;
 }
 D_P21(power) {
-  I n=getFloorZ(r); V vt,v=cpy1(ll); T t[2]; t[0]=T(v);
-  DDO(i,n) {
+  Z ns=getFloorZ(r); U n=ns;
+  if(ns<0) { del(l); ERR("Number of iterations must not be negative"); }
+  V vt,v=cpy1(ll); T t[2]; t[0]=T(v);
+  DDO(i,(U)n) {
     t[1]=t[0]; t[0]=apply_T(l,1,t); if (t[0]==t[1]) break;
     vt=v; T(v)=t[0]; P(v)=MALLOC(t_sizeof(T(v)));
     apply1_P(v, l, vt); FREE(P(vt)); if (err) break;
@@ -40,7 +42,7 @@ D_P21(power) {
       RegM pop=start_A(a,1, 1<<REG_LOOP);
       ASM(a, PUSH,REG_ARG1,-);
       asm_load(a,T(v),REG_RES,REG_ARG1);
-      ASM(a, MOV,REG_LOOP,REG_ARG0); I label=a->l;
+      ASM(a, MOV,REG_LOOP,REG_ARG0); U label=a->l;
       apply_A_full(a,l,1,t);
       ASM(a, LOOP,label-a->l,-);
       ASM(a, POP,REG_ARG1,-);
@@ -54,12 +56,14 @@ D_P21(power) {
   if (!err) mv_P(p,v); FREE(P(v));
 }
 D_T22(power) {
-  I n=getFloorZ(r);
-  DO(i,n) { T llt=apply2_T(l,ll,rr); if(llt==ll)break; ll=llt; }
+  Z n=getFloorZ(r); if(n<0) return E_t;
+  DO(i,(U)n) { T llt=apply2_T(l,ll,rr); if(llt==ll)break; ll=llt; }
   return ll;
 }
 D_P22(power) {
-  I n=getFloorZ(r); ll=cpy1(ll); DO(i,n) ll=apply2_d(l,ll,cpy(rr));
+  Z ns=getFloorZ(r); U n=ns;
+  if(ns<0) { del(l); del(r); ERR("Number of iterations must not be negative"); }
+  ll=cpy1(ll); DO(i,n) ll=apply2_d(l,ll,cpy(rr));
   del(rr); mv_P(p,ll); FREE(P(ll));
 }
 
@@ -79,7 +83,7 @@ D_A21(while) {
   RegM u=a->u; Reg i_=a->i[0], i = a->o<NO_REG ? a->o : i_;
   if (i!=i_ && i_<NO_REG) ASM(a, MOV,i,i_);
   a->o=a->i[0]=i; protect_input(a->i, &a->u);
-  I jstart=a->l; ASM(a, JMP,0,-); I label=a->l;
+  U jstart=a->l; ASM(a, JMP,0,-); U label=a->l;
   apply_A(a, l, 1, &ll);
   if (a->i[0] >= NO_REG) a->i[0] = a->o;
   else if (a->o != a->i[0]) ASM(a, MOV,a->i[0],a->o);
